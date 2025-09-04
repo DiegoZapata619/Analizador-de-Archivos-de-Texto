46303388f78d0d2135071f617bf597f5aa38146b
DiegoZapata619 <A24216385@alumnos.uady.mx>
2025-09-04 15:51:10 -0600

    Add files via upload

---
+++ v3.py
@@ -0,0 +1,31 @@
+import re 
+import os
+
+def escribir_coincidencias(nombre_texto_final, coincidencias):
+    with open(nombre_texto_final, "w", encoding="utf-8") as f:
+        for coincidencia in coincidencias:
+            f.write(coincidencia + "\n")
+
+#Entrada del nombre del archivo
+archivo= input("Ingrese la dirección del archivo de texto: ")
+#Verifica si el archivo existe antes de intentar leerlo
+while True:
+    if os.path.isfile(archivo):
+        break
+    else:
+        print("El archivo no existe. Intente de nuevo.")
+        archivo= input("Ingrese la dirección del archivo de texto: ")
+
+#Búsqueda de una expresión regular en el archivo
+expresionRegular = input("Ingrese una expresión regular para buscar en el archivo: ")
+nombre_texto_final = input("Ingrese el nombre del archivo donde se guardarán las coincidencias: ")
+
+# Lee el archivo y busca las coincidencias. Almacena las coincidencias en una lista con el nombre del documento final.
+with open(archivo, "r", encoding="utf-8") as f:
+    contenido = f.read()
+coincidencias = re.findall(expresionRegular, contenido,re.IGNORECASE) # Agrega re.IGNORECASE para hacer la búsqueda insensible a mayúsculas y minúsculas
+if coincidencias:
+    escribir_coincidencias(nombre_texto_final, coincidencias)
+    print(f"Se encontraron {len(coincidencias)} coincidencias. Se han guardado en '{nombre_texto_final}'.")
+else:
+    print("No se encontraron coincidencias.")
